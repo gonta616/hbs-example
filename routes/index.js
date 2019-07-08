95f812ab38e715ec46f6e465c7aa06ebad17b1b9
kgonjo <kgonjo@yahoo-corp.jp>
2019-07-08 11:26:47 +0900

    Add condition, iterate

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const isShowTitle = req.query.showtitle;
   res.render('index', { 
     title: 'Express',
-    author: { firstName: 'Gonjo', lastName: 'Kazuya' }
+    author: { firstName: 'Gonjo', lastName: 'Kazuya' },
+    members: [
+      {name: 'member1', role: 'member'},
+      {name: 'member2', role: 'member'},
+      {name: 'member3', role: 'admin'},
+    ],
+    showTitle: isShowTitle
   });
 });
 
